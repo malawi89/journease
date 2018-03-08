5b62250aad7bd528f8ebe0f109a93a19954a8b7f
David Morley <davidkmorley@gmail.com>
2018-03-08 13:14:45 +0000

    added autocomplete to accomm/activity forms, changed autocomplete of journey to destination location, added fields to edit user form

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -1,5 +1,5 @@
 class Journey < ApplicationRecord
   belongs_to :trip
-  geocoded_by :departure_location
-  after_validation :geocode, if: :will_save_change_to_departure_location?
+  geocoded_by :arrival_location
+  after_validation :geocode, if: :will_save_change_to_arrival_location?
 end
