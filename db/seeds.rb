b3333d41a893a91e33b7fe68ae755644b641a5eb
David Morley <davidkmorley@gmail.com>
2018-03-09 12:33:02 +0000

    added snazzy map and added login button to navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
+def rand_in_range(from, to)
+  rand * (to - from) + from
+end
+
+def rand_time(from, to=Time.now)
+  Time.at(rand_in_range(from.to_f, to.to_f))
+end
+
 puts "Wiping database..."
 User.destroy_all
 Trip.destroy_all
@@ -15,8 +23,8 @@ puts "Wiped!"
 
 puts "Seeding database..."
 
-test = User.create!(email: 'test@test.com', password: '123456', password_confirmation: '123456', first_name: 'Testy', last_name: 'McTest')
-other = User.create!(email: 'test1@test.com', password: '123456', password_confirmation: '123456', first_name: 'Other', last_name: 'McOtherson')
+test = User.create!(username: "poundcake", email: 'bobomb@test.com', password: '123456', password_confirmation: '123456', first_name: 'Testy', last_name: 'McTest')
+other = User.create!(username: "testymctestface", email: 'test1@test.com', password: '123456', password_confirmation: '123456', first_name: 'Other', last_name: 'McOtherson')
 
 10.times do
   user = User.new(
@@ -33,8 +41,8 @@ end
 3.times do
   trip = Trip.new(
     name: Faker::Name.name,
-    start_date: 6.days.from_now,
-    end_date: 10.days.from_now
+    start_date: rand_time(6.days.from_now),
+    end_date: rand_time(10.days.from_now)
     )
   trip.save!
   TripUser.create!(trip: trip, user: test)
@@ -43,8 +51,8 @@ end
 3.times do
   trip = Trip.new(
     name: Faker::Name.name,
-    start_date: 6.days.from_now,
-    end_date: 10.days.from_now
+    start_date: rand_time(6.days.from_now),
+    end_date: rand_time(10.days.from_now)
     )
   trip.save!
   TripUser.create!(trip: trip, user: other)
@@ -58,7 +66,9 @@ trip_ids = Trip.all.map(&:id)
   trip_id: trip_ids.sample,
   sub_category: "train",
   departure_location: ["138 kingsland road, London", "121 Winslow Road, Wingrave", "Brighton Pier, Brighton", "Windsor Castle", "350 Queens Quay W, Toronto", "Blantyre, Malawi", "Paris, France", "Athens, Greece"].sample,
-  arrival_location: ["138 kingsland road, London", "121 Winslow Road, Wingrave", "Brighton Pier, Brighton", "Windsor Castle", "350 Queens Quay W, Toronto", "Blantyre, Malawi", "Paris, France", "Athens, Greece", "Sparta, Greece", "Istanbul, Turkey"].sample
+  arrival_location: ["138 kingsland road, London", "121 Winslow Road, Wingrave", "Brighton Pier, Brighton", "Windsor Castle", "350 Queens Quay W, Toronto", "Blantyre, Malawi", "Paris, France", "Athens, Greece", "Sparta, Greece", "Istanbul, Turkey"].sample,
+  start_time: rand_time(6.days.from_now),
+    end_time: rand_time(7.days.from_now)
     )
   journey.save!
 end
