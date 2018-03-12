3607586b98090f4317d387fba02f0a25ae82cad9
Rito Sisinni <ritosisinni@yahoo.gr>
2018-03-12 16:23:38 +0000

    Added documents upload done

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -1,6 +1,6 @@
 class Journey < ApplicationRecord
+  mount_uploader :booking_documents, PhotoUploader
   belongs_to :trip
   geocoded_by :arrival_location
   after_validation :geocode, if: :will_save_change_to_arrival_location?
-  mount_uploader :photo, PhotoUploader
 end
