99b168c2fe09bec3ad22fb5892768ce42380559e
David Morley <davidkmorley@gmail.com>
2018-03-15 14:51:02 +0000

    changed forms for user registration and edit

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,11 +5,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:email, :password, :username, :first_name, :last_name, :photo, :photo_cache)
+    params.require(:user).permit(:email, :password, :photo, :photo_cache)
   end
 
   def account_update_params
-    params.require(:user).permit(:email, :password, :username, :first_name, :last_name, :photo, :photo_cache, :current_password)
+    params.require(:user).permit(:email, :password, :photo, :photo_cache, :current_password)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :username, presence: true, uniqueness: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+
   validates :email, presence: true, uniqueness: true
 
   has_many :trip_users, dependent: :destroy
