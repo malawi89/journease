624a6cc73e2e1a50d2d9544f7fe3e0c5b10cd343
David Morley <davidkmorley@gmail.com>
2018-04-11 09:56:28 +0100

    fixed form formatting, defaulted date selection to first day of trip, banned selection of dates outside of trip

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,11 +2,17 @@ import flatpickr from "flatpickr";
 import 'flatpickr/dist/themes/material_green.css'
 import rangePlugin from 'flatpickr/dist/plugins/rangePlugin'
 
+var startDate = document.querySelector(".flatpickr-start-date").innerHTML;
+var endDate = document.querySelector(".flatpickr-end-date").innerHTML;
 flatpickr(".flatpicker", {
   altInput: true,
-  enableTime: true
+  enableTime: true,
+  defaultDate: startDate,
+  minDate: startDate,
+  maxDate: endDate
 });
 flatpickr("#start_date", {
   altInput: true,
   "plugins": [new rangePlugin({ input: "#end_date"})]
 });
+
